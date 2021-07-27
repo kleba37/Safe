170af31847f226067bb5029740539a625fdc66fc
scagroup <scagroup@yandex.ru>
2021-07-27 15:38:21 +0300

    Clear index

--- index.php
+++ index.php
@@ -9,10 +9,6 @@ include_once("library/Processor.php");
 include_once("library/Variant.php");
 include_once("library/Password.php");
 
-//$timer = new \Timer();
-
-//var_dump($timer->get());
-
 $connector = new ConnectDB("test", "test", "test", "mysql");
 $password = new Password(11);
 $safe = new Safe($connector, $password);
@@ -20,15 +16,6 @@ $safe = new Safe($connector, $password);
 $variant = new Variant();
 $processor = new Processor($safe, $variant);
 print_r("Перебрали: ".$processor->attempts());
-//$viwe = new SafeViwe($safe);
-//$viwe->viwe();
 
-//$data = new Data();
-//$data->add("attempts");
-
-//if($_POST['submit']) {
-//    $safe->attempts($data);
-//    $data->get();
-//};
 
 ?>
